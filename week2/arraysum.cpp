97e55cce74e7c5fdac7f6eb5aca85eaf67bc7b1c
unknown <kaamyagaur@gmail.com>
2021-09-23 17:41:39 +0530

    arraysum

---
+++ week2/arraysum.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int test;
+   cin>>test;
+   while(test--){
+      int n;
+      cin>>n;
+      int arr[n];
+      bool flag = False;
+      for(int i=0; i<n; i++) cin >> arr[i];
+      for(int i=0;i<n-2;i++){
+         for(int j=i+1; j<n-1;j++){
+            for(int k=i+2;k<n;k++){
+               if(arr[i]+arr[j]==arr[k]){
+                  cout<<i<<","<<j<<","<<k;
+                  flag=True;
+                  break;
+               }}}}
+      if(!flag){
+          cout<< "No sequemce found";
+}}}
+
+
